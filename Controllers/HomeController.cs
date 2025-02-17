8619183f171d98b9cd25e5c8cceb878ca714c827
emil-ivanov92 <107074220+emil-ivanov92@users.noreply.github.com>
2025-02-17 19:51:32 +0200

    Main pages added 17.02.2025
    Main pages added + code clean up to error 404 and contacts.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,4 +33,14 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Events()
+    {
+        return View();
+    }
 }
