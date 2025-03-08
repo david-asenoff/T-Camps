40f4c5a30531df122350afac1b88c974298006f6
david-asenoff <david.asenoff@gmail.com>
2025-03-08 16:05:52 +0200

    Add UpcomingEventsViewComponent in the footer

---
+++ ViewComponents/UpcomingEventsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using T_Camps.Data;
+using T_Camps.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace T_Camps.ViewComponents
+{
+    public class UpcomingEventsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UpcomingEventsViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var events = await _context.Events.Select(e => new EventViewModel
+            {
+                Date = e.StartDate.ToString("dd MMMM yyyy"),
+                Title = e.Name,
+                Url = Url.Action("Details", "Events", new { id = e.Id })
+            }).ToListAsync();
+
+            return View(events);
+        }
+    }
+}
---
+++ ViewModels/EventViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using T_Camps.Models;
+
+namespace T_Camps.ViewModels
+{
+    public class EventViewModel
+    {
+        public string Date { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
