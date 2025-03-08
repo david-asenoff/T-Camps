5a87833766dc7566482c5051fe11a90c75f0b269
david-asenoff <david.asenoff@gmail.com>
2025-03-08 16:30:38 +0200

    Add EventController

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using T_Camps.Data;
+using T_Camps.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace T_Camps.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var upcomingEvents = await _context.Events
+                .Where(e => e.StartDate > DateTime.Now)
+                .ToListAsync();
+
+            return View(upcomingEvents);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var eventDetail = await _context.Events
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if (eventDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventDetail);
+        }
+    }
+}
