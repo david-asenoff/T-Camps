e8ebc305a4d8edca71c309e9c48f0ec8f1c523ba
emil-ivanov92 <107074220+emil-ivanov92@users.noreply.github.com>
2025-03-12 20:08:18 +0200

    Add social 12.03.2025
    Added company info as e service instead of model.
    Added a new font-family because of you tube icon.

---
+++ Services/CompanyService.cs
@@ -0,0 +1,24 @@
+﻿using T_Camps.Data;
+using T_Camps.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace T_Camps.Services
+{
+    public class CompanyService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CompanyService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Company> GetCompanyAsync()
+        {
+            return await _context.Companies
+                .Include(c => c.Missions)
+                .Include(c => c.Services)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
