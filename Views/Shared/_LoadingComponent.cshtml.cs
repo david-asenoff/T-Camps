90796ff9677f3f3f84571aa2e0945acb0a2bfd73
emil-ivanov92 <107074220+emil-ivanov92@users.noreply.github.com>
2025-03-29 23:06:57 +0200

    AddSpinner 29.03.2025
    Add a loading element. Add site.js in _Layout (also added in brand - AddGallery).

---
+++ Views/Shared/_LoadingComponent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace T_Camps.Views.Shared
+{
+    public class _LoadingComponentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
